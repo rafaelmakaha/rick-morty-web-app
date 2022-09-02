9b752fdb1e77ccac4cec8a48f156a2e743dd2808
Rafael Makaha <rafael.makaha@gmail.com>
2022-09-02 20:01:25 -0300

    improve: Remove unused styles
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
